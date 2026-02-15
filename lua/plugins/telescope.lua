daee503cf8ee5a6feb4958ca6a5cd6d7e2653115
Collabo93 <blessing.timo@gmail.com>
2026-02-15 19:51:33 +0100

    feat: added keybing support shift j and k for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,9 +8,16 @@ return {
     },
 
     config = function()
+        local actions = require("telescope.actions")
         require("telescope").setup({
             defaults = {
                 file_ignore_patterns = { "node_modules", "%.git/" },
+                mappings = {
+                    i = {
+                        ["J"] = actions.move_selection_next,
+                        ["K"] = actions.move_selection_previous,
+                    },
+                },
             },
         })
     end,
