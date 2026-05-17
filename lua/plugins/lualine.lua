3184b6f7aff68b3884a3a814cf461bcf97364913
Collabo93 <blessing.timo@gmail.com>
2026-05-17 12:06:42 +0200

    style<lualine>: color and placement ajdustments

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,36 +1,38 @@
 local c = require("catppuccin.palettes").get_palette("mocha")
 
+local function gradient(accent)
+    return {
+        b = { bg = c.surface0, fg = accent },
+        c = { bg = "NONE", fg = c.subtext1 },
+    }
+end
+
 local catppuccin_lualine = {
     normal = {
         a = { bg = c.blue, fg = c.base, gui = "bold" },
-        b = { bg = "NONE", fg = c.text },
-        c = { bg = "NONE", fg = c.subtext1 },
+        b = gradient(c.blue).b,
+        c = gradient(c.blue).c,
     },
-
     insert = {
         a = { bg = c.red, fg = c.base, gui = "bold" },
-        b = { bg = "NONE", fg = c.text },
-        c = { bg = "NONE", fg = c.text },
+        b = gradient(c.red).b,
+        c = gradient(c.red).c,
     },
-
     visual = {
         a = { bg = c.mauve, fg = c.base, gui = "bold" },
-        b = { bg = "NONE", fg = c.text },
-        c = { bg = "NONE", fg = c.text },
+        b = gradient(c.mauve).b,
+        c = gradient(c.mauve).c,
     },
-
     replace = {
         a = { bg = c.green, fg = c.base, gui = "bold" },
-        b = { bg = "NONE", fg = c.text },
-        c = { bg = "NONE", fg = c.text },
+        b = gradient(c.green).b,
+        c = gradient(c.green).c,
     },
-
     command = {
         a = { bg = c.yellow, fg = c.base, gui = "bold" },
-        b = { bg = "NONE", fg = c.text },
-        c = { bg = "NONE", fg = c.text },
+        b = gradient(c.yellow).b,
+        c = gradient(c.yellow).c,
     },
-
     inactive = {
         a = { bg = c.mantle, fg = c.overlay1, gui = "bold" },
         b = { bg = "NONE", fg = c.overlay1 },
@@ -41,10 +43,18 @@ local catppuccin_lualine = {
 require("lualine").setup({
     options = {
         theme = catppuccin_lualine,
-        disabled_filetypes = { 'alpha' },
+        disabled_filetypes = { "alpha" },
     },
-
     sections = {
-        lualine_x = { 'filetype' },
+        lualine_b = { "branch" },
+        lualine_c = {
+            {
+                "filename",
+                path = 1,
+            }
+        },
+        lualine_x = {},
+        lualine_y = { "diagnostics" },
+        lualine_z = { "filetype" },
     },
 })
