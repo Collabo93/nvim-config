4c11b0554ceaa36de350c80d49ebe9245603c4fc
Collabo93 <blessing.timo@gmail.com>
2026-05-24 16:16:59 +0200

    feat: replaced blink and dependecies with native autocompletion

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -1,12 +1,8 @@
 vim.pack.add({
     -- alpha-nvim, oil
     { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-    --blink-cmp, luaSnip
-    { src = "https://github.com/rafamadriz/friendly-snippets" },
     -- telescope,todo-comments
     { src = "https://github.com/nvim-lua/plenary.nvim" },
-    -- blink-cmp
-    { src = "https://github.com/saghen/blink.lib" },
 
     -- **plugins**
     -- **prio 1000**
@@ -23,16 +19,11 @@ vim.pack.add({
     },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter-context" },
     { src = "https://github.com/goolord/alpha-nvim" },
-    {
-        src = "https://github.com/saghen/blink.cmp",
-        version = "v1.10.2"
-    },
     { src = "https://github.com/github/copilot.vim" },
     { src = "https://github.com/sindrets/diffview.nvim" },
     { src = "https://github.com/tpope/vim-fugitive" },
     { src = "https://github.com/nvim-lualine/lualine.nvim" },
     { src = "https://github.com/mawkler/modicator.nvim" },
-    { src = "https://github.com/L3MON4D3/LuaSnip" },
     { src = "https://github.com/nvim-telescope/telescope.nvim" },
     { src = "https://github.com/folke/todo-comments.nvim" },
     { src = "https://github.com/akinsho/toggleterm.nvim" },
@@ -42,17 +33,16 @@ vim.pack.add({
 })
 
 require("vim._core.ui2").enable({})
+vim.cmd("packadd nvim.undotree")
 
 require('plugins.devicons')
 require('plugins.treesitter')
 require('plugins.alpha')
-require('plugins.blink')
 require('plugins.catppuccin')
 require('plugins.diffview')
 require('plugins.gitsigns')
 require('plugins.lualine')
 require('plugins.modicator')
-require('plugins.luasnip')
 require('plugins.telescope')
 require('plugins.toogleterm')
 require('plugins.oil')
